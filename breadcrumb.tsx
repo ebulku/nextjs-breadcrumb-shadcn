4b1d8e4aa1f2258e5466fe2293454178a7eb236c
ebulku <endri.bulku@live.com>
2024-11-14 17:48:34 +0100

    Update breadcrumb.tsx
    Added BreadcrumbPage and some other min fixes

--- breadcrumb.tsx
+++ breadcrumb.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -5,42 +7,54 @@ import {
     BreadcrumbList,
     BreadcrumbSeparator,
     BreadcrumbEllipsis,
-} from "@/components/ui/breadcrumb"
+    BreadcrumbPage,
+} from '@/components/ui/breadcrumb';
 import { usePathname } from 'next/navigation';
-import Link from 'next/link';
 import { Fragment } from 'react';
+import { HomeIcon } from 'lucide-react';
+import Link from 'next/link';
 
 const MAX_BREADCRUMB_LENGTH: number = 3;
 
 const BreadcrumbLogic = () => {
     const paths: string = usePathname();
-    const separator: string = "/";
-    const pathNames: string[] = paths.split(separator).filter(path => path !== "");
-    
+    const separator: string = '/';
+    const pathNames: string[] = paths
+        .split(separator)
+        .filter((path) => path !== '');
+
     return (
         <Breadcrumb>
             <BreadcrumbList>
                 <BreadcrumbItem>
                     <BreadcrumbLink asChild>
-                        <Link href="/">Home</Link>
+                        <Link href="/">
+                            <HomeIcon className="h-4 w-4" />
+                        </Link>
                     </BreadcrumbLink>
                 </BreadcrumbItem>
-                {pathNames.length > MAX_BREADCRUMB_LENGTH && <BreadcrumbEllipsis />}
+                {pathNames.length > MAX_BREADCRUMB_LENGTH && (
+                    <BreadcrumbEllipsis />
+                )}
                 {pathNames.slice(-MAX_BREADCRUMB_LENGTH).map((link, index) => {
-                    const href: string = `/${pathNames.slice(0, -MAX_BREADCRUMB_LENGTH + index + 1).join('/')}`;
-                    const linkName: string = link[0].toUpperCase() + link.slice(1);
+                    const href: string = `/${pathNames
+                        .slice(0, -MAX_BREADCRUMB_LENGTH + index + 1)
+                        .join('/')}`;
+                    const linkName: string =
+                        link[0].toUpperCase() + link.slice(1);
                     const isLastPath: boolean = pathNames.length === index + 1;
-                    
+
                     return (
                         <Fragment key={index}>
-                            <BreadcrumbSeparator>&gt;</BreadcrumbSeparator>
+                            <BreadcrumbSeparator />
                             <BreadcrumbItem>
-                                <BreadcrumbLink asChild>
-                                    {!isLastPath ?
-                                        <Link href={href}>{linkName}</Link> :
-                                        <span>{linkName}</span>
-                                    }
-                                </BreadcrumbLink>
+                                {!isLastPath ? (
+                                    <BreadcrumbLink asChild>
+                                        <Link href={href}>{linkName}</Link>
+                                    </BreadcrumbLink>
+                                ) : (
+                                    <BreadcrumbPage>{linkName}</BreadcrumbPage>
+                                )}
                             </BreadcrumbItem>
                         </Fragment>
                     );
